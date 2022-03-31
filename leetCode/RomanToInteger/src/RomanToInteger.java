4727dc3805688e2383c2bf44bb42607499ad1b48
s.timofeev <java.junior@icloud.com>
2022-03-31 19:50:47 +0300

    2

---
+++ leetCode/RomanToInteger/src/RomanToInteger.java
@@ -0,0 +1,70 @@
+import java.util.HashMap;
+
+/**
+ * * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+ * * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * For example, 2 is written as II in Roman numeral, just two one's added together.
+ * 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+ * * Roman numerals are usually written largest to smallest from left to right.
+ * However, the numeral for four is not IIII. Instead, the number four is written as IV.
+ * Because the one is before the five we subtract it making four.
+ * The same principle applies to the number nine, which is written as IX.
+ * There are six instances where subtraction is used:
+ *
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ * Given a roman numeral, convert it to an integer.
+ */
+public class RomanToInteger {
+    public static void main(String[] args) {
+        romanToInt ("IX");
+        romanToInt ("III");
+        romanToInt ("IV");
+        romanToInt ("XXIII");
+
+
+    }
+
+    public static int romanToInt(String s) {
+
+        HashMap<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I',1);
+        romanMap.put('V',5);
+        romanMap.put('X',10);
+        romanMap.put('L',50);
+        romanMap.put('C',100);
+        romanMap.put('D',500);
+        romanMap.put('M',1000);
+
+        char[] romanArray=s.toCharArray();
+        int[] decimalArray=new int[romanArray.length+1];
+        int decimalSum=0;
+
+        for (int i=0; i<romanArray.length;i++){
+            decimalArray[i]=romanMap.get(romanArray[i]);
+        }
+
+        for (int i=0; i<decimalArray.length-1;i++){
+           int currentValue=decimalArray[i];
+           int nextValue=decimalArray[i+1];
+
+           if(currentValue>=nextValue){
+               decimalSum+=currentValue;
+           }else if (currentValue<nextValue){
+            decimalSum-=currentValue;
+           }
+        }
+
+        System.out.println(decimalSum);
+
+    return decimalSum;
+    }
+}
---
+++ leetCode/longestCommonPrefix/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] str = {"cir", "car"};
+        Solution s = new Solution();
+        System.out.println(s.longestCommonPrefix(str));
+    }
+
+
+}
